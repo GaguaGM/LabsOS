9d59f29cd6e7fadf647c7319e61a402e028d87e9
GaguaGM <43414877+GaguaGM@users.noreply.github.com>
2018-12-16 22:13:57 +0300

    I have some questions

--- lab3-fifo/main.c
+++ lab3-fifo/main.c
@@ -8,27 +8,32 @@
 
 #define FIFO "./file"
 #define BUFFER_SIZE 8
-char* ctime (const time_t * timer);
 
-int main(){
+
+void time_now(char *);
+
+int main(int argc,char * argv[]){
 	pid_t pid;
 	char * buffer = (char *)calloc(BUFFER_SIZE,sizeof(char));
 	mkfifo(FIFO,0777);
 	pid = fork();
 	if (pid == 0){
-		printf("CHILD:HIIIIIII\n");
 		int fd = open(FIFO,O_WRONLY);
-		time_t data = time(0);
-		time (&data);
-		sprintf(buffer,"%s",ctime(&data));
+		time_now(buffer);
 		write(fd,buffer,BUFFER_SIZE);
 		close(fd);
 	}
 	else {
-		printf("PARENT:HELLOUUUU\n");
 		int fd = open(FIFO,O_RDONLY);
 		read(fd,buffer,BUFFER_SIZE);
 		printf("From proc: %s\n",buffer);
 		close(fd);
 		}
+}
+
+
+void time_now(char * str){
+	time_t timer = time(0);
+	struct tm timeval = *localtime(&timer);
+	sprintf(str, "[%.2d:%.2d:%.2d]\n", timeval.tm_hour, timeval.tm_min, timeval.tm_sec);
 }
