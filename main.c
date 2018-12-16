0c102cccf6a29cf1b756b7d562d71109c03a4892
GaguaGM <43414877+GaguaGM@users.noreply.github.com>
2018-12-16 22:39:37 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define FIFO "./file"
+#define BUFFER_SIZE 80
+
+
+void time_now(char *);
+
+int main(int argc,char * argv[]){
+	pid_t pid;
+	char * buffer = (char *)calloc(BUFFER_SIZE,sizeof(char));
+	mkfifo(FIFO,0777);
+	pid = fork();
+	if (pid == 0){
+		int fd = open(FIFO,O_WRONLY);
+		time_now(buffer);
+		write(fd,buffer,BUFFER_SIZE);
+		close(fd);
+	}
+	else {
+		int fd = open(FIFO,O_RDONLY);
+		read(fd,buffer,BUFFER_SIZE);
+		printf("From proc: %s\n",buffer);
+		close(fd);
+		}
+}
+
+
+void time_now(char * str){
+	time_t timer = time(0);
+	struct tm timeval = *localtime(&timer);
+	sprintf(str, "[%.2d:%.2d:%.2d]\n", timeval.tm_hour, timeval.tm_min, timeval.tm_sec);
+}
