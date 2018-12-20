b98112b8bb09b7c95aaf2fdbdac8e9151cbaafa0
GaguaGM <43414877+GaguaGM@users.noreply.github.com>
2018-12-20 19:02:21 +0300

    added mutex

---
+++ lab5-sem/consumer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+
+struct sembuf sem_lock = {0,-1,0}, sem_open = {0,1,0};
+int main(){
+	char * time_spam;
+	key_t key = ftok("/tmp",'a');
+	int shmid = (shmget(2002,32,0666));
+	int semid = (semget(key,1,0666));
+	if (semid == -1){
+		printf("error\n");
+		exit(0);
+	}
+	if(shmid == -1){
+		printf("cant open mem\n");
+		exit(0);
+	}
+	if((time_spam = shmat(shmid,time_spam,0)) == (char*)-1)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+
+
+	while(1){
+		semop(semid,&sem_lock,1);
+		printf("%s",time_spam);
+		semop(semid,&sem_open,1);
+		sleep(1);
+	}
+	return 0;
+}
