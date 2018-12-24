e00c81131b8757e74df7308755e4f52ef503648e
GaguaGM <43414877+GaguaGM@users.noreply.github.com>
2018-12-25 00:15:40 +0300

    fix

--- lab4-shmem/Consumer.c
+++ lab4-shmem/Consumer.c
@@ -16,7 +16,7 @@ int main(){
 		printf("cant open memory\n");
 		exit(0);
 	}
-	if ((time_spam = shmat(shmid,time_spam,0)) == (char*)-1){
+	if ((time_spam = shmat(shmid,0,0)) == (char*)-1){
 		printf("Error\n");
 		exit(0);
 	}
